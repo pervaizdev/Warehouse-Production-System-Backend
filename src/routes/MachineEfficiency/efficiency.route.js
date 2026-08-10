b001c05d726208e76e721fb07ddd19adf40f2db3
pervaizdev <pervaizdev01@gmail.com>
2026-08-10 16:21:02 +0500

    code

--- src/routes/MachineEfficiency/efficiency.route.js
+++ src/routes/MachineEfficiency/efficiency.route.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const efficiencyController = require('../../controller/MachineEfficiency/efficiency.controller');
 const authMiddleware = require('../../middleware/auth.middleware');
+
 router.get('/machine-efficiency', authMiddleware, efficiencyController.getMachineEfficiency);
+router.get('/filter-options', authMiddleware, efficiencyController.getFilterOptions);
+router.get('/machine/:machineId', authMiddleware, efficiencyController.getMachineDrilldown);
+router.get('/order/:orderNum', authMiddleware, efficiencyController.getOrderDrilldown);
 
 module.exports = router;
